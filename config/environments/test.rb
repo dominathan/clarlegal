1e1264fc7a2d0a173d0425c79875dcd612483814
Nathan Hall <nathan.mh@gmail.com>
2014-06-17 11:33:41 -0500

    added bcrypt, tests for passwords, signup, SSL security, usercontroller additions, additional routes, and tests to accompany the creation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Claregal::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  #added for testing efficiency
+  ActiveModel::SecurePassword.min_cost = true
 end
