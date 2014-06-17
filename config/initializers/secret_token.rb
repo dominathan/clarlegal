26a7e285a2b2be34e89ef1d79253d6851c0d8e8c
Nathan Hall <nathan.mh@gmail.com>
2014-06-17 08:57:24 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Claregal::Application.config.secret_key_base = 'e1c204836d796b70f76f60cf01a2e0641aae90c445f7e531cb65f039ba965223697e5cb9824c7e2947567503ad61d50d0d21ac1c472cc5ad0cff123defaf4c1a'
