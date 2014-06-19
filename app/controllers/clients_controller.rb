a7039dee8dd8ca5d8a6c5104d283cb10178716e8
Nathan Hall <nathan.mh@gmail.com>
2014-06-19 11:03:35 -0500

    added clientcreate action, validation of certain client objects, and belongs_to user

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,4 +4,27 @@ class ClientsController < ApplicationController
     @client = Client.new
   end
 
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      flash[:success] = "Client added sucessfully"
+      redirect_to @client
+    else
+      render 'new'
+      flash[:error] = "Please fix the fields below"
+    end
+  end
+
+      #FROM USER CREATE------
+          #   def create
+          #   @user = User.new(user_params)
+          #   if @user.save
+          #     sign_in @user
+          #     flash[:success] = "Welcome"
+          #     redirect_to @user
+          #   else
+          #     render 'new'
+          #   end
+          # end
+
 end
