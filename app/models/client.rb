5e44ef80aa6335c17ba86b438a3178806f2d5831
Nathan Hall <nathan.mh@gmail.com>
2014-06-20 18:33:14 -0500

    changed models for validation of client name and email, updated edit form to show all fields, updated show form to show all fields of client

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,8 @@ class Client < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, presence: true
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :client_name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i || ""
+  validates :client_email, format: { with: VALID_EMAIL_REGEX }
 
 end
