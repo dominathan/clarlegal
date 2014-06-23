de3963b77908655caea9b9391c051e93b61a6d15
Nathan Hall <nathan.mh@gmail.com>
2014-06-23 12:07:35 -0500

    added base fees controller

--- app/controllers/fees_controller.rb
+++ app/controllers/fees_controller.rb
@@ -1,2 +1,25 @@
 class FeesController < ApplicationController
+  before_action :signed_in_user
+
+  def new
+    @fee = Fee.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+    def fee_params
+        params.require(:fee).permit(:fee_type, :high_estimate, :medium_estimate,
+                                    :low_estimate, :payment_likelihood, :retainer,
+                                    :cost_estimate)
+    end
+
 end
