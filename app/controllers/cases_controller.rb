d93072d7de87f52ad3366100ba31cf48e992ab07
Nathan Hall <nathan.mh@gmail.com>
2014-06-23 10:12:27 -0500

    updated cases controller for indexing and updating, updated views for cases and clients

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -8,24 +8,37 @@ class CasesController < ApplicationController
   end
 
   def index
-    @case = Case.all
+    @client = current_user.clients.find(params[:client_id])
+    if @client.cases.exists?
+      @case = @client.cases.all
+    end
   end
 
   def create
     current_client = Client.find(params[:client_id])
     @case = current_client.cases.new(case_params)
     if @case.save
-      flash[:success] = "Client added sucessfully"
-      redirect_to clients_path
+      flash[:success] = "Case added sucessfully"
+      redirect_to client_cases_path
     else
       render 'new'
     end
   end
 
-  def udpate
+  def update
+    current_client = Client.find(params[:client_id])
+    client_case = current_client.cases.find(params[:id])
+    if client_case.update_attributes(case_params)
+      flash[:success] = "Updated case matter information"
+      redirect_to client_cases_path
+    else
+      render 'edit'
+    end
   end
 
   def edit
+    current_client = Client.find(params[:client_id])
+    @case = current_client.cases.find(params[:id])
   end
 
   def destroy
