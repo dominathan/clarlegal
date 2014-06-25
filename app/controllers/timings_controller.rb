c386450d3a915a095097ffefaa4dcafceddd0915
Nathan Hall <nathan.mh@gmail.com>
2014-06-25 11:07:41 -0500

    added show and edit function to timing... need to bifurcate open&close with keydates...make new model

--- app/controllers/timings_controller.rb
+++ app/controllers/timings_controller.rb
@@ -19,6 +19,32 @@ class TimingsController < ApplicationController
     end
   end
 
+  def show
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @timing = Timing.find(params[:id])
+  end
+
+  def edit
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @timing = Timing.find(params[:id])
+  end
+
+  def update
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @timing = Timing.find(params[:id])
+    if @timing.update_attributes(timing_params)
+      flash[:success] = "Dates Updated"
+      redirect_to client_case_path(@client, @case)
+    else
+      render 'edit'
+    end
+  end
+
+
+
 
   private
 
