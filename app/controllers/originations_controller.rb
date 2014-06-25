b2dab6e91c0a0f7ebda8cc6e2988bed897054a4e
Nathan Hall <nathan.mh@gmail.com>
2014-06-25 11:28:00 -0500

    added origination controller with edit, new, show views

---
+++ app/controllers/originations_controller.rb
@@ -0,0 +1,2 @@
+class OriginationsController < ApplicationController
+end
---
+++ spec/helpers/originations_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the OriginationsHelper. For example:
+#
+# describe OriginationsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe OriginationsHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
