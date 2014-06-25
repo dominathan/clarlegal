0a0a3466f19dea72f3faf23776113496bf431b9f
Nathan Hall <nathan.mh@gmail.com>
2014-06-25 14:20:54 -0500

    added check contoller with actions, added views. rocking and rolling

---
+++ app/controllers/checks_controller.rb
@@ -0,0 +1,53 @@
+class ChecksController < ApplicationController
+  before_action :signed_in_user
+
+  def new
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @check = Check.new
+  end
+
+  def create
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @check = @case.check.new(check_params)
+    if @check.save
+      flash[:success] = "Check Dates Added Successfully."
+      redirect_to client_case_path(@client, @case)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @check = Check.find(params[:id])
+  end
+
+  def edit
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @check = Check.find(params[:id])
+  end
+
+  def update
+    @case = Case.find(params[:case_id])
+    @client = Client.find(params[:client_id])
+    @check = Check.find(params[:id])
+    if @check.update_attributes(check_params)
+      flash[:success] = "Updated Check Dates Successfully"
+      redirect_to client_case_path(@client, @case)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def check_params
+      params.require(:check).permit(:conflict_check, :retention_letter)
+    end
+
+
+end
