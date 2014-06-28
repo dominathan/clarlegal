115b10f664ea84d29a31a2069b53c6af4a3bfafc
Nathan Hall <nathan.mh@gmail.com>
2014-06-28 16:31:31 -0500

    add staffing path to header, visual adjustments to practicegroup index, add staffings controller with all actions except destroy

---
+++ app/controllers/staffings_controller.rb
@@ -0,0 +1,54 @@
+class StaffingsController < ApplicationController
+  before_action :signed_in_user
+
+  def new
+    @staffing = Staffing.new
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+  end
+
+  def index
+    @staffing = Staffing.all
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @staff = @lawfirm.staffings.new(staffing_params)
+    if @staff.save
+      flash[:success] = "Staff Added Successfully."
+      redirect_to user_lawfirm_staffings_path(@user, @lawfirm)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @staff = Staffing.find(params[:id])
+  end
+
+  def edit
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @staff = Staffing.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @staff = @lawfirm.staffings.find(params[:id])
+    if @staff.update_attributes(staffing_params)
+      flash[:success] = "Staff Updated Successfully"
+      redirect_to user_lawfirm_staffings_path(@user, @lawfirm)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def staffing_params
+      params.require(:staffing).permit(:first_name, :last_name, :position)
+    end
+
+end
