c9d3ef1a102a987b1acaf37ce6c1a534bfb5136d
Nathan Hall <nathan.mh@gmail.com>
2014-06-28 09:32:35 -0500

    added redirect to practice groups index in header

--- app/controllers/practicegroups_controller.rb
+++ app/controllers/practicegroups_controller.rb
@@ -2,18 +2,36 @@ class PracticegroupsController < ApplicationController
   before_action :signed_in_user
 
   def new
+    @prac_group = Practicegroup.new
+    @lawfirm = Lawfirm.find(params[:id])
   end
 
   def index
+    @user = User.find(params[:user_id])
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @prac_group = Practicegroup.all
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @prac_group = @lawfirm.practicegroups.new(practicegroup_params)
+    if @prac_group.save
+      flash[:success] = "Practice Group Added Succesfully."
+      redirect_to user_lawfirm_path(@user,@lawfirm)
+    else
+      render 'new'
+    end
   end
 
   def show
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @prac_group = Practicegroup.find(params[:id])
   end
 
   def edit
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @prac_group = Practicegroup.find(params[:id])
   end
 
   def update
