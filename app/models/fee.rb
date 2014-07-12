72369f40b69562286b48501c4bf59751af8a88d0
Nathan Hall <nathan.mh@gmail.com>
2014-07-12 09:53:44 -0500

    must have high medium low fee estimates or it will throw an error on bar chart generation

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -2,6 +2,9 @@ class Fee < ActiveRecord::Base
   belongs_to :case
 
   validates :case_id, presence: true
+  validates :high_estimate, presence: true
+  validates :medium_estimate, presence: true
+  validates :low_estimate, presence: true
 
 
   FEE_PAYMENT_LIKELIHOOD = ['High','Medium','Low']
