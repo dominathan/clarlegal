dfb225d6ef258dcd2b2d5cccc2e895afe43e2c57
Nathan Hall <nathan.mh@gmail.com>
2014-07-23 10:43:22 -0500

    made unique positions only visible  on staff for case

--- app/models/staffing.rb
+++ app/models/staffing.rb
@@ -4,4 +4,10 @@ class Staffing < ActiveRecord::Base
 
   validates :lawfirm_id, presence: true
 
+  def self.all_positions
+    position_list = Staffing.all.collect! { |person| person.position }
+    position_list =  position_list.uniq!
+  end
+
+
 end
