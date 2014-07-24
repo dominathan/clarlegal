fdbc3a363030a48e1d2f7058b1ae711c00135383
Nathan Hall <nathan.mh@gmail.com>
2014-07-24 11:19:57 -0500

    added catch for blank shit, but i think we can add a proc under accepts nested attributes that rejects the filing if it is blank

--- app/models/case.rb
+++ app/models/case.rb
@@ -3,20 +3,28 @@ class Case < ActiveRecord::Base
   belongs_to :client
   belongs_to :user
 
-
+  has_many :fee
   has_many :fees
   accepts_nested_attributes_for :fees
+
+  has_many :staff
   has_many :staffs
   accepts_nested_attributes_for :staffs
+
+  has_many :timing
   has_many :timings
   accepts_nested_attributes_for :timings
+
   has_many :originations
   accepts_nested_attributes_for :originations
+
   has_many :venues
   accepts_nested_attributes_for :venues
+
   has_many :checks
   accepts_nested_attributes_for :checks
-  has_many :closeout
+
+  has_many :closeouts
 
 
 
