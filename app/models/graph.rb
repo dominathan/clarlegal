f405c8847d8322aeaf0d7fba0c9fb2bfeba59d10
Nathan Hall <nathan.mh@gmail.com>
2014-08-21 15:11:59 -0500

    1) all views changed the legend titles from Gross to Net 2)graph.rb, line 42, all_dates.compact to strip the array of nil values if nil values are in there, this will let ruby run the comparison

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -39,7 +39,7 @@ class Graph < ActiveRecord::Base
   #start year should be five years prior to the most recent collection fee_received
   def self.closeout_years
     all_dates = Closeout.all.order(:date_fee_received).collect { |cl| cl.date_fee_received }
-    start_year = all_dates.sort.last.year-4
+    start_year = all_dates.compact.sort.last.year-4
     [start_year, start_year+1,start_year+2,start_year+3,start_year+4]
   end
 
