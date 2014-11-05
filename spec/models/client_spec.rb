ee75d0deef0c13735a8fa4592269d43ba9d4ba09
Nathan Hall <nathan.mh@gmail.com>
2014-11-04 18:09:37 -0600

    Fixed Client Rspec test

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -5,7 +5,8 @@ describe Client do
   let(:user) { FactoryGirl.create(:user) }
   before { @client = user.clients.build(first_name: "jo", last_name: "jo",
                      email: "jo@jo.com", zip_code: 63333, user_id: user.id,
-                     city: "Aust", state: 'AL') }
+                     city: "Aust", state: 'AL', phone_number: '205-205-2005',
+                     street_address: "205 Hell Way") }
 
   subject { @client }
 
@@ -17,6 +18,8 @@ describe Client do
   it { should respond_to(:state)}
   it { should respond_to(:zip_code)}
   it { should respond_to(:user_id)}
+  it { should respond_to(:phone_number)}
+  it { should respond_to(:street_address)}
   its(:user) { should eq user}
 
   it { should be_valid }
