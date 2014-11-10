c4b2b4022288c669ad8fbc0d83360fa05393c893
Nathan Hall <nathan.mh@gmail.com>
2014-11-10 15:37:04 -0600

    another silly mandrill test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,21 +77,20 @@ Claregal::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'clarlegal.herokuapp.com' }
+  #config.action_mailer.default_url_options = { :host => 'clarlegal.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  @heroku_username = ENV["MANDRILL_HEROKU_USERNAME"]
-  @heroku_apikey = ENV["MANDRILL_HEROKU_APIKEY"]
+
 
    config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
     :enable_starttls_auto => true,
-    :user_name => @heroku_username,
-    :password  => @heroku_apikey,
+    :user_name => 'app28721402@heroku.com',
+    :password  => 'j1SYoPsxtGfJOcFn2CfKbA',
     :authentication => 'plain',
-    :domain => 'herokuapp.com'
+    :domain => 'heroku.com'
   }
 
 end
