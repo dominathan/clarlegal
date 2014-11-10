3c868a70ac19c63be591c4736923251676758139
Nathan Hall <nathan.mh@gmail.com>
2014-11-10 16:03:47 -0600

    changed look and feel and redirect upon successful validation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,9 +3,10 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
-      flash[:success] = "Account activated!"
+      flash[:success] = "We're excited to have you! Please enter your law firm's information if it has already been registered.\n
+                        If you are the first user, enter the required information in the field below to protect your data."
       sign_in user
-      redirect_to joinlawfirm_path
+      redirect_to new_user_lawfirm_path(user)
     else
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
