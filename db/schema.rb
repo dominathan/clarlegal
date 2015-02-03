6830e627abd20309880a98a0e2a731254b7e6409
Nathan Hall <nathan.mh@gmail.com>
2015-02-03 12:07:53 -0600

    added default value for staff hours worked --- still an issue if remove the default 0 and hit save.  Defaults back to nil, need to not save items on nil

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203170647) do
+ActiveRecord::Schema.define(version: 20150203180144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -247,8 +247,8 @@ ActiveRecord::Schema.define(version: 20150203170647) do
     t.string   "position"
     t.integer  "percent_utilization"
     t.integer  "staffing_id"
-    t.integer  "hours_actual"
-    t.integer  "hours_expected"
+    t.integer  "hours_actual",        default: 0
+    t.integer  "hours_expected",      default: 0
   end
 
   add_index "staffs", ["case_id"], name: "index_staffs_on_case_id", using: :btree
