7965b89d85685a36d30590dabcae12d276afcb9f
Ian White <ianwhite@utexas.edu>
2015-02-08 17:39:19 -0600

    Add two passing tests for #perfom

--- spec/workers/reminder_emailer_worker_spec.rb
+++ spec/workers/reminder_emailer_worker_spec.rb
@@ -5,37 +5,54 @@ Sidekiq::Testing.fake!
 include SpecHelper
 
 describe ReminderEmailWorker do
-  describe '#get_primary_emails' do
-    before {
-      10.times do
-        plaintiff = Faker::Name.name
-        defendant = Faker::Name.name
-        opposing_attorney = Faker::Name.name
-        judge_list = ["Houston L. Brown","Donald Blankenship","Joseph Boohaker","Elisabeth French", "Helen Shores Lee", "Robert Vance"]
-        Case.create!(
-            client_id: Random.rand(1..20),
-            practicegroup_id: Random.rand(1..20),
-            primary_email: Faker::Internet.email,
-            updated_at: rand_time(1.year.ago, 6.months.ago),
-          )
-      end
+  before {
+    10.times do
+      plaintiff = Faker::Name.name
+      defendant = Faker::Name.name
+      opposing_attorney = Faker::Name.name
+      judge_list = ["Houston L. Brown","Donald Blankenship","Joseph Boohaker","Elisabeth French", "Helen Shores Lee", "Robert Vance"]
+      Case.create!(
+          client_id: Random.rand(1..20),
+          practicegroup_id: Random.rand(1..20),
+          primary_email: Faker::Internet.email,
+          updated_at: rand_time(1.year.ago, 6.months.ago),
+        )
+    end
 
-      5.times do
-        plaintiff = Faker::Name.name
-        defendant = Faker::Name.name
-        opposing_attorney = Faker::Name.name
-        judge_list = ["Houston L. Brown","Donald Blankenship","Joseph Boohaker","Elisabeth French", "Helen Shores Lee", "Robert Vance"]
-        Case.create!(
-            client_id: Random.rand(1..20),
-            practicegroup_id: Random.rand(1..20),
-            primary_email: Faker::Internet.email,
-            updated_at: rand_time(5.months.ago),
-          )
-      end
-    }
+    5.times do
+      plaintiff = Faker::Name.name
+      defendant = Faker::Name.name
+      opposing_attorney = Faker::Name.name
+      judge_list = ["Houston L. Brown","Donald Blankenship","Joseph Boohaker","Elisabeth French", "Helen Shores Lee", "Robert Vance"]
+      Case.create!(
+          client_id: Random.rand(1..20),
+          practicegroup_id: Random.rand(1..20),
+          primary_email: Faker::Internet.email,
+          updated_at: rand_time(5.months.ago),
+        )
+    end
+  }
+
+
+  describe '#get_primary_emails' do
     it 'only returns primary email addresses of cases that have not been updated in at least 6 months' do
       @primary_emails_array = ReminderEmailWorker.new.get_primary_emails
       @primary_emails_array.size.should eq(10)
+    end
+  end
+
+  describe '#perform' do
+    before {
+      Sidekiq::Worker.clear_all
+    }
+
+    it 'has no jobs queued initially' do
+      ReminderEmailWorker.jobs.size.should eq(0)
+    end
+
+    it 'adds a job to the queue' do
+      ReminderEmailWorker.perform_async
+      ReminderEmailWorker.jobs.size.should eq(1)
     end
   end
 end
