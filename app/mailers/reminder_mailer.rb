b1575b8f79752b206322a4067895524ec2935436
Ian White <ianwhite@utexas.edu>
2015-02-08 18:26:33 -0600

    Begin default reminder mailer views

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -6,7 +6,7 @@ class ReminderMailer < ActionMailer::Base
 
   def send_email_reminder(primary_emails_array)
     primary_emails_array.each do |primary_email_address|
-      # mail to: primary_email_address, subject: ""
+      # mail to: primary_email_address, subject: "Default Reminder"
       # puts email_address
       logger.info { "#{primary_email_address}" }
     end
