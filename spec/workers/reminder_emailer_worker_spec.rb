6d99fde3ee11a1fcba754ea63bb436099498e940
Ian White <ianwhite@utexas.edu>
2015-02-08 18:12:01 -0600

    Remove duplicate array

--- spec/workers/reminder_emailer_worker_spec.rb
+++ spec/workers/reminder_emailer_worker_spec.rb
@@ -6,11 +6,12 @@ include SpecHelper
 
 describe ReminderEmailWorker do
   before {
+    judge_list = ["Houston L. Brown","Donald Blankenship","Joseph Boohaker","Elisabeth French", "Helen Shores Lee", "Robert Vance"]
+
     10.times do
       plaintiff = Faker::Name.name
       defendant = Faker::Name.name
       opposing_attorney = Faker::Name.name
-      judge_list = ["Houston L. Brown","Donald Blankenship","Joseph Boohaker","Elisabeth French", "Helen Shores Lee", "Robert Vance"]
       Case.create!(
           client_id: Random.rand(1..20),
           practicegroup_id: Random.rand(1..20),
@@ -23,7 +24,6 @@ describe ReminderEmailWorker do
       plaintiff = Faker::Name.name
       defendant = Faker::Name.name
       opposing_attorney = Faker::Name.name
-      judge_list = ["Houston L. Brown","Donald Blankenship","Joseph Boohaker","Elisabeth French", "Helen Shores Lee", "Robert Vance"]
       Case.create!(
           client_id: Random.rand(1..20),
           practicegroup_id: Random.rand(1..20),
