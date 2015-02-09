85049d77b361529af2d22e15fe005f95028d80c9
Ian White <ianwhite@utexas.edu>
2015-02-08 18:00:35 -0600

    Refactor #get_primary_emails in reminder_email_worker.rb

--- app/workers/reminder_email_worker.rb
+++ app/workers/reminder_email_worker.rb
@@ -7,12 +7,8 @@ class ReminderEmailWorker
   sidekiq_options retry: false
 
   def get_primary_emails
-    @cases = Case.where("updated_at < :default", {:default => 6.months.ago})
-    @primary_emails_array = @cases.map do |c|
-      c.primary_email
-    end
-
-    return @primary_emails_array
+    # Why use .pluck? see http://rubyinrails.com/2014/06/rails-pluck-vs-select-map-collect/
+    return Case.where("updated_at < :default", {:default => 6.months.ago}).pluck(:primary_email)
   end
 
   def perform
