2c82c029ddd51efb36ea26f32e7c841aefcfe211
Nathan Hall <nathan.mh@gmail.com>
2015-02-10 14:41:18 -0600

    1) _form_new_case - fixing forms 2) graph.rb - temp file for mail

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -638,4 +638,27 @@ class Graph < ActiveRecord::Base
     return all_fee_types.zip(final).map { |name,values|  { 'name' => name, 'data' => values } }.to_json
   end
 
+
+#----------------------------------REMOVE
+  # def self.get_all_unupdated_case_ids(date_since_last_update)
+  #   Case.select { |ca| ca.fees.last.updated_at <= date_since_last_update ||
+  #                   ca.timings.last.updated_at <= date_since_last_update ||
+  #                   ca.updated_at <= date_since_last_update }
+  #       .collect(&:id)
+  # end
+
+  # def self.prim_emails_and_case_ids(var)
+  #   email_and_ids = []
+  #   var.each do |id|
+  #     email_and_ids << [Case.find(id).primary_email, id]
+  #   end
+  #   vals = email_and_ids.group_by { |item| item[0] }
+  #   keys = vals.keys
+  #   keys.each do |key|
+  #     vals[key] = vals[key].map { |item| item[1] }
+  #   end
+  #   vals
+  # end
+
+
 end
