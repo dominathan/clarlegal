b2337b0f1a49f6fb883e6435fdaf4b007724488b
Nathan Hall <nathan.mh@gmail.com>
2015-02-10 23:24:33 -0600

    changing :host on produciton.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Claregal::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'clarlegal.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'clarlegal.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
