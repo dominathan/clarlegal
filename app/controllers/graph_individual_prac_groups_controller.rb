aa59349fb46213361560faf29a0b2e38ead188a4
Nathan Hall <nathan.mh@gmail.com>
2015-03-09 17:11:08 -0500

    graph_drilldowns handles fee_estimates and updates

--- app/controllers/graph_individual_prac_groups_controller.rb
+++ app/controllers/graph_individual_prac_groups_controller.rb
@@ -6,7 +6,6 @@ class GraphIndividualPracGroupsController < ApplicationController
     @med_fast = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_fast','medium_estimate')
     @low_fast = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_fast','low_estimate')
     @cost_fast = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_fast','cost_estimate').map {|i| i * -1}
-    #@retainer_fast = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_fast','retainer')
     @referral_fast = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_fast','medium_referral').map {|i| i * -1}
 
 
@@ -14,7 +13,6 @@ class GraphIndividualPracGroupsController < ApplicationController
     @med_expected = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_expected','medium_estimate')
     @low_expected = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_expected','low_estimate')
     @cost_expected = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_expected','cost_estimate').map {|i| i * -1}
-    #@retainer_expected = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_expected','retainer')
     @referral_expected = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_expected','medium_referral').map {|i| i * -1}
 
 
@@ -22,7 +20,6 @@ class GraphIndividualPracGroupsController < ApplicationController
     @med_slow = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_slow','medium_estimate')
     @low_slow = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_slow','low_estimate')
     @cost_slow = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_slow','cost_estimate').map {|i| i * -1}
-    #@retainer_slow = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_slow','retainer')
     @referral_slow = Graph.fee_estimate_by_year_by_pg(current_user,params[:id],'estimated_conclusion_slow','medium_referral').map {|i| i * -1}
 
     @total_recovery = Graph.closeout_by_year_pg(current_user,params[:id],'total_recovery')
@@ -37,6 +34,6 @@ class GraphIndividualPracGroupsController < ApplicationController
 
     @fee_types_med = Graph.fee_estimate_by_fee_type_pg(current_user,params[:id],"medium_estimate","estimated_conclusion_expected")
   end
-#---------------------Begin Accelerated Revenue by PG---------------------------------
+
 
 end
