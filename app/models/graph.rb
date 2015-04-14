836ab010df8faa4cc8706154547b75dcd3bdbe65
Nathan Hall <nathan.mh@gmail.com>
2015-04-14 09:30:36 -0500

    added open: true to graph method to prevent confusion in presentation

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -658,6 +658,7 @@ class Graph < ActiveRecord::Base
   #Sum fee_estimate regardless of case being opened of closed with dates of choice
   def self.projected_amount_earned_time_frame(user,fee_estimate,timing_estimate,starting_date,ending_date)
     user.lawfirm.cases.joins(:timings,:fees)
+                      .where(open: true)
                       .where('fees.created_at = (SELECT MAX(created_at) FROM fees p group by case_id having p.case_id = fees.case_id)')
                       .where('timings.created_at = (SELECT MAX(created_at) FROM timings d group by case_id having d.case_id = timings.case_id)')
                       .where("#{timing_estimate} >= :start_date AND #{timing_estimate} <= :end_date",
